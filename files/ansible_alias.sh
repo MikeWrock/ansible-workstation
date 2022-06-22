26efa31df1c90ed27da8f8ff5457a6c50c028612
Michael Wrock <michael.wrock@picknik.ai>
2022-06-22 16:53:13 -0700

    added bashrc configuration

---
+++ files/ansible_alias.sh
@@ -0,0 +1,3 @@
+alias ansup='sudo bash ~/ansible-workstation/ansible-update.sh'
+alias ansb4='dconf dump / > /tmp/before.txt'
+alias ansaft='dconf dump / > /tmp/after.txt && diff /tmp/before.txt /tmp/after.txt'
