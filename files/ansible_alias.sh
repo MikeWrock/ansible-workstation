5a64365f605c844626870f98135d52b2d5827e55
Michael Wrock <michael.wrock@picknik.ai>
2022-06-22 19:03:50 -0700

    added script shebang

--- files/ansible_alias.sh
+++ files/ansible_alias.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 alias ansup='sudo bash ~/ansible-workstation/ansible-update.sh'
 alias ansb4='dconf dump / > /tmp/before.txt'
 alias ansaft='dconf dump / > /tmp/after.txt && diff /tmp/before.txt /tmp/after.txt'
